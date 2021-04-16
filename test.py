fd9f83d8a161eae707dec9502ae2ad65f58ef624
Betty Chitty <42243427+DataBettyLBRB@users.noreply.github.com>
2021-04-15 21:28:25 -0400

    test app to scratch folder

--- test.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-if __name__ == "__main__":
-    path = 'static/resumes/Chitty_Developer_Resume_2021.pdf'
-
-    pdf = open(path, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdf)
-
-    print(pdfReader.numPages)
